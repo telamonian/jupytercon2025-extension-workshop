4c883b21c7cf6747ee6146e24768d73e44f16829
telamonian <telamonian@users.noreply.github.com>
2025-11-03 11:20:13 -0800

    added widget

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { ICommandPalette } from '@jupyterlab/apputils';
 
 import { requestAPI } from './request';
+import { ImageCaptionMainAreaWidget } from './widget';
 
 /**
  * Initialization data for the jupytercon2025-extension-workshop extension.
@@ -12,7 +14,13 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupytercon2025-extension-workshop:plugin',
   description: 'A JupyterLab extension that displays a random image and caption.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (
+    // The activation method receives dependencies in the order they are specified in
+    // the "requires" parameter above:
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+  ) => {
     console.log('JupyterLab extension jupytercon2025-extension-workshop is activated!');
 
     requestAPI<any>('hello')
@@ -24,6 +32,22 @@ const plugin: JupyterFrontEndPlugin<void> = {
           `The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
         );
       });
+
+    //Register a new command:
+    const command_id = 'image-caption:open';
+    app.commands.addCommand(command_id, {
+      execute: () => {
+        // When the command is executed, create a new instance of our widget
+        const widget = new ImageCaptionMainAreaWidget();
+
+        // Then add it to the main area:
+        app.shell.add(widget, 'main');
+        return widget;
+      },
+      label: 'View a random image & caption'
+    });
+
+    palette.addItem({ command: command_id, category: 'Tutorial' });
   }
 };
 
